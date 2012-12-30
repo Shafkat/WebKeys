5ff9fb46b943d0001e59355c942c4806edd01e48
Shafkat <shafkatulhaque@live.ca>
2012-12-29 21:02:17 -0500

    Updating the repo with the new version of webkeys I worked on. This is big change after months..but I worked on it locally. Looks pretty good on now on a Playbook. Some bugs to be fixed.

--- javascript/webkeys.js
+++ javascript/webkeys.js
@@ -1,258 +1,203 @@
-var smallRow1 = ['q','w','e','r','t','y','u','i','o','p'];
-var smallRow2 = ['a','s','d','f','g','h','j','k','l'];
-var smallRow3 = ['Shift','z','x','c','v','b','n','m', 'Shift'];
+var _leftKeysCaps = [ 'Q','W','E','R','T','A','S','D','F','G','Alt','Z','X','C','V' ],
+	_rightKeysCaps = [ 'Y','U','I','O','P','H','J','K','L','Del','B','N','M','$','Enter' ],
+	_leftSymbolKeys = [ '#','1','2','3','(','*','4','5','6','/','Alt','7','8','9','?' ],
+	_rightSymbolKeys = [ ')','_','-','+','@',':',';','\'','"','Del','!',',','.','$','Enter' ],
+	_bottomLeftRow = [ 'Shift','0','Space' ],
+	_bottomRightRow = [ 'Space','Sym','Shift' ],
 
-var smallChars = new Array();
-smallChars[0] = smallRow1;
-smallChars[1] = smallRow2;
-smallChars[2] = smallRow3;
+	_capsKeyArray = [ _leftKeysCaps, _rightKeysCaps ],
+	_symKeyArray = [ _leftSymbolKeys, _rightSymbolKeys ],
 
-var capRow1 = ['Q','W','E','R','T','Y','U','I','O','P'];
-var capRow2 = ['A','S','D','F','G','H','J','K','L'];
-var capRow3 = ['Shift','Z','X','C','V','B','N','M','Shift'];
+	_tableArray = [ 'LeftKeyTable', 'RightKeyTable' ],
 
-var capChars = new Array();
-capChars[0] = capRow1;
-capChars[1] = capRow2;
-capChars[2] = capRow3;
+	capsOn = false,
+	symbolOn = false;
 
-var state;
-
-head.js("https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js","./javascript/ui.js","./javascript/touch.js", function (){
-$('.touchBox').draggable({revert:true});
-	$(".dropArea").droppable({
-    drop: function( event, ui ) {
-        //$(ui.draggable).remove();
-        $(this).css({'border':'#777 solid 3px','background':'#eee'});
-    },
-    over: function(event, ui) {
-        $(this).css({'border':'#a33 solid 3px','background':'#faa'});
-    },
-    out: function (event, ui){
-        $(this).css({'border':'#777 solid 3px','background':'#eee'});
-    }
-	});
-});
-
-//Dynamically generate the table to hold the keys
-function loadStructure()
+loadKeys = function( array )
 {
-	var maincontainer = document.getElementById('main_container');
-	var dynaTable1 = document.createElement('table');
-	var dynaTable2 = document.createElement('table');
-	var dynaTable3 = document.createElement('table');
-	var dynaTable4 = document.createElement('table');
-	
-	var row1 = document.createElement('tr');
-	var row2 = document.createElement('tr');
-	var row3 = document.createElement('tr');
-	var row4 = document.createElement('tr');
-	
-	$(dynaTable1).attr('id','main_table1');
-	$(dynaTable1).attr('align','center');
+	var table,
+		counter1,
+		counter2;
 
-	$(dynaTable2).attr('id','main_table2');
-	$(dynaTable2).attr('align','center');
-
-	$(dynaTable3).attr('id','main_table3');
-	$(dynaTable3).attr('align','center');
-	
-	$(dynaTable4).attr('id','main_table4');
-	$(dynaTable4).attr('align','center');
-	
-	$(row1).attr('id','main_table1_row1');
-	
-	$(row2).attr('id','main_table2_row2');
-	
-	$(row3).attr('id','main_table3_row3');
-	
-	$(row4).attr('id','main_table3_row4');
-	
-	maincontainer.appendChild(dynaTable1);
-	maincontainer.appendChild(dynaTable2);
-	maincontainer.appendChild(dynaTable3);
-	maincontainer.appendChild(dynaTable4);
-
-	dynaTable1.appendChild(row1);
-	dynaTable2.appendChild(row2);
-	dynaTable3.appendChild(row3);
-	dynaTable4.appendChild(row4);
-	
-	loadKeys();
-}
-
-function loadKeys()
-{
-	state = 'low';
-
-	var row1 = document.getElementById('main_table1_row1');
-	var row2 = document.getElementById('main_table2_row2');
-	var row3 = document.getElementById('main_table3_row3');
-	
-	$(row1).empty();
-	$(row2).empty();
-	$(row3).empty();
-	
-	var rows = [row1, row2, row3];
-	
-	for(var i = 0; i < smallChars.length; i++)
+	for( counter1 = 0; counter1 < array.length; counter1++ )
 	{
-		for(var j = 0; j < smallChars[i].length; j++)
+		table = document.getElementById( _tableArray[ counter1 ] );
+
+		for( counter2 = 0; counter2 < array[ counter1 ].length; counter2++ )
 		{
-			var cell = document.createElement('td');
-			var key = document.createElement('div');
-			
-			var keyName = document.createElement('div');
-			
-			$(key).attr('id', smallChars[i][j]);
-			$(key).attr('class', 'touchBox');
-			
-			keyName.innerHTML = smallChars[i][j];
-			keyName.setAttribute('class', 'keyLabel');
-			
-			if(state == 'low' && smallChars[i][j] == 'Shift'){
-				$(key).attr('ontouchstart', 'loadKeysUpperCase();');
+			var tr;
+
+			if( counter2 % 5 === 0 ) {
+				var trTmp = document.createElement( 'tr' );
+				table.appendChild( trTmp );			
+				tr = trTmp;
 			}
-			else
-			{
-				$(key).attr('ontouchstart', 'writeTHIS(this.id.toString());');
-				$(key).attr('ontouchend', '$("#"+this.id.toString()).css("background-color","#999")');
-			}
-			
-			key.appendChild(keyName);
-			rows[i].appendChild(cell);
-			cell.appendChild(key);
+
+			var tmpCell = document.createElement( 'td' );
+			var key = document.createElement( 'div' );
+			key.id = array[ counter1 ][ counter2 ];
+			key.innerHTML = array[ counter1 ][ counter2 ];
+			key.setAttribute( 'class', 'key' );
+			key.setAttribute( 'ontouchend', 'onKeyPress( this )' );
+			//key.setAttribute( 'onclick', 'onKeyPress( this )' );
+
+			tmpCell.appendChild( key );
+			tr.appendChild( tmpCell );
 		}
 	}
-	
-	bottomRow();
-	
-	head.js("https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js","./javascript/ui.js","./javascript/touch.js", function (){
-	$('.touchBox').draggable({revert:true});
-	});
-	
+
+	loadBottomRowKeys();
 }
 
-function loadKeysUpperCase()
+loadBottomRowKeys  = function()
 {
-	state = 'up';
-	
-	var row1 = document.getElementById('main_table1_row1');
-	var row2 = document.getElementById('main_table2_row2');
-	var row3 = document.getElementById('main_table3_row3');
+	var bottomTable = document.createElement( 'table' ),
+		bottomTableBody = document.createElement( 'tbody' ),
+		counter,
+		trow = document.createElement( 'tr' );
 
-	$(row1).empty();
-	$(row2).empty();
-	$(row3).empty();
-	
-	var rows = [row1, row2, row3];
-	
-	for(var i = 0; i < capChars.length; i++)
-	{
-		for(var j = 0; j < capChars[i].length; j++)
+		for( counter = 0; counter < _bottomLeftRow.length; counter++ )
 		{
-			var cell = document.createElement('td');
-			var key = document.createElement('div');
-			
-			var keyName = document.createElement('div');
-			
-			$(key).attr('id', capChars[i][j]);
-			$(key).attr('class', 'touchBox');
-			
-			keyName.innerHTML = capChars[i][j];
-			keyName.setAttribute('class', 'keyLabel');
-			
-			if(state == 'up' && capChars[i][j] == 'Shift'){
-				$(key).attr('ontouchstart', 'loadKeys();');
+			var tmpCell = document.createElement( 'td' );
+
+			var key = document.createElement( 'div' );
+			key.id = _bottomLeftRow[ counter ];
+			key.innerHTML = _bottomLeftRow[ counter ];
+			key.setAttribute( 'class', 'key' );
+			key.setAttribute( 'ontouchend', 'onKeyPress( this )' );
+
+			if( key.innerHTML === 'Space' ) {
+				key.style.width = '160px';
 			}
-			else
-			{
-				$(key).attr('ontouchstart', 'writeTHIS(this.id.toString());');
-				$(key).attr('ontouchend', '$("#"+this.id.toString()).css("background-color","#999")');
-			}
-			
-			key.appendChild(keyName);
-			rows[i].appendChild(cell);
-			cell.appendChild(key);
+
+			tmpCell.appendChild( key );
+			trow.appendChild( tmpCell );
 		}
-	}
-	
-	bottomRow();
-	
-	head.js("https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js","./javascript/ui.js","./javascript/touch.js", function (){
-	$('.touchBox').draggable({revert:true});
-	});
-	
+
+		bottomTableBody.appendChild( trow );
+		bottomTable.appendChild( bottomTableBody );
+		document.getElementById( 'main_container' ).children[ 0 ].appendChild( bottomTable );
+
+		bottomTable = document.createElement( 'table' );
+		bottomTableBody = document.createElement( 'tbody' );
+		trow = document.createElement( 'tr' );
+		for( counter = 0; counter < _bottomRightRow.length; counter++ )
+		{
+			var tmpCell = document.createElement( 'td' );
+
+			var key = document.createElement( 'div' );
+			key.id = _bottomRightRow[ counter ];
+			key.innerHTML = _bottomRightRow[ counter ];
+			key.setAttribute( 'class', 'key' );
+			key.setAttribute( 'ontouchend', 'onKeyPress( this )' );
+			//key.setAttribute( 'onclick', 'onKeyPress( this )' );
+
+			if( key.innerHTML === 'Space' ) {
+				key.style.width = '160px';
+			}
+
+			tmpCell.appendChild( key );
+			trow.appendChild( tmpCell );
+		}
+
+		bottomTableBody.appendChild( trow );
+		bottomTable.appendChild( bottomTableBody );
+		document.getElementById( 'main_container' ).children[ 1 ].appendChild( bottomTable );
 }
 
-function writeTHIS(text)
+onKeyPress = function( key )
 {
-	$("#"+text).css("background-color","#000000");
+	var textArea = document.getElementById( 'textArea' );
 
-	var box = document.getElementById('textfield');
-	
-	if(text == 'spacer')
-	{
-		box.innerHTML += ' ';
-	}
-	else if(text == 'return')
-	{
-		box.innerHTML += '<br />';
-	}
-	else if(text == 'delete')
-	{
-		var str = box.innerHTML;
-		var newStr = str.substring(0, str.length-1);
-		box.innerHTML = '';
-		box.innerHTML = newStr;
-	}
-	else
-	{
-		box.innerHTML += text;
-	}
+	if( key.innerHTML === 'Shift' || key.innerHTML === 'Return' || key.innerHTML === 'Space' || key.innerHTML === 'Del' || key.innerHTML === 'Alt' ) {
+		
+		if( key.innerHTML === 'Shift' ) {
+			if( capsOn ) {
+				capsOn = false;
+				document.getElementById( 'Shift' ).setAttribute( 'class', 'capsOff' );
+			} else {
+				capsOn = true;
+				document.getElementById( 'Shift' ).setAttribute( 'class', 'capsOn' );
+			}
+		}
 
+		if( key.innerHTML === 'Space' ) {
+			textArea.textContent += ' ';
+		}
+
+		if( key.innerHTML === 'Del' ) {
+			if( textArea.textContent.length >= 0 ) {
+				textArea.textContent = textArea.textContent.substring( 0, textArea.textContent.length - 1 );
+			}
+		}
+
+		if( key.innerHTML === 'Alt' ) {
+
+			var Ltable = document.getElementById( 'LeftKeyTable' ),
+				Rtable = document.getElementById( 'RightKeyTable' ),
+				tableArray = [ Ltable, Rtable ],
+				charCounter,
+				tmpTr;
+
+			if( !symbolOn ) {
+				symbolOn = true;
+
+				for( var i = 0; i < tableArray.length; i++ )
+				{
+					charCounter = 0;
+
+					for( var counter = 0; counter < tableArray[ i ].childNodes.length - 1; counter++ )
+					{
+						tmpTr = tableArray[ i ].childNodes[ counter + 1 ];
+						for( var counter2 = 0; counter2 < tmpTr.childNodes.length; counter2++ )
+						{
+							tmpTr.childNodes[ counter2 ].childNodes[ 0 ].id = _symKeyArray[ i ][ charCounter ];
+							tmpTr.childNodes[ counter2 ].childNodes[ 0 ].textContent = _symKeyArray[ i ][ charCounter ];
+							tmpTr.childNodes[ counter2 ].childNodes[ 0 ].setAttribute( 'class', 'key' );
+							tmpTr.childNodes[ counter2 ].childNodes[ 0 ].setAttribute( 'ontouchend', 'onKeyPress( this )' );
+							//tmpTr.childNodes[ counter2 ].childNodes[ 0 ].setAttribute( 'onclick', 'onKeyPress( this )' );
+							charCounter++;
+						}
+					}
+				}
+			} else {
+				symbolOn = false;
+
+				for( var i = 0; i < tableArray.length; i++ )
+				{
+					charCounter = 0;
+
+					for( var counter = 0; counter < tableArray[ i ].childNodes.length - 1; counter++ )
+					{
+						tmpTr = tableArray[ i ].childNodes[ counter + 1 ];
+						for( var counter2 = 0; counter2 < tmpTr.childNodes.length; counter2++ )
+						{
+							tmpTr.childNodes[ counter2 ].childNodes[ 0 ].id = _capsKeyArray[ i ][ charCounter ];
+							tmpTr.childNodes[ counter2 ].childNodes[ 0 ].textContent = _capsKeyArray[ i ][ charCounter ];
+							tmpTr.childNodes[ counter2 ].childNodes[ 0 ].setAttribute( 'class', 'key' );
+							tmpTr.childNodes[ counter2 ].childNodes[ 0 ].setAttribute( 'ontouchend', 'onKeyPress( this )' );
+							//tmpTr.childNodes[ counter2 ].childNodes[ 0 ].setAttribute( 'onclick', 'onKeyPress( this )' );
+							charCounter++;
+						}
+					}
+				}
+			}
+		}
+
+	} else {
+		if( capsOn ) {
+			textArea.textContent += key.innerHTML;	
+		} else {
+			textArea.textContent += key.innerHTML.toLowerCase();
+		}
+		
+	}
 }
 
-function bottomRow()
-{
-	var cellSpace = document.createElement('td');
-	var cellDelete = document.createElement('td');
-	var cellReturn = document.createElement('td');
-	var row4 = document.getElementById('main_table3_row4');
-	
-	$(row4).empty();
-	
-	var space = document.createElement('div');
-	space.setAttribute('class', 'touchBox');
-	space.setAttribute('id', 'spacer');
-	space.innerHTML = 'Space';
-	space.setAttribute('ontouchstart', 'writeTHIS(this.id.toString());');
-	
-	space.setAttribute('ontouchend', '$("#"+this.id.toString()).css("background-color","#999")');
-	
-	var del = document.createElement('div');
-	del.setAttribute('class', 'touchBox');
-	del.setAttribute('id', 'delete');
-	del.innerHTML = 'Delete';
-	del.setAttribute('ontouchstart', 'writeTHIS(this.id.toString());');
 
-	del.setAttribute('ontouchend', '$("#"+this.id.toString()).css("background-color","#999")');
 
-	var returnKey = document.createElement('div');
-	returnKey.setAttribute('class', 'touchBox');
-	returnKey.setAttribute('id', 'return');
-	returnKey.innerHTML = 'Return';
-	returnKey.setAttribute('ontouchstart', 'writeTHIS(this.id.toString());');
-	
-	returnKey.setAttribute('ontouchend', '$("#"+this.id.toString()).css("background-color","#999")');
-	
-	row4.appendChild(cellSpace);
-	row4.appendChild(cellReturn);
-	row4.appendChild(cellDelete);
-	
-	cellSpace.appendChild(space);
-	
-	cellDelete.appendChild(del);
-	
-	cellReturn.appendChild(returnKey);
-}
+
+
+
+
+
